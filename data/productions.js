bc60c0a95ab618bfec2b7cadf04e080121dc74c6
Clément Roig <clm.roig@gmail.com>
2019-04-17 20:45:19 +0200

    new fonts, new css, bigger logo, js data on

---
+++ data/productions.js
@@ -0,0 +1,29 @@
+os_productions = [
+	{
+		name: "A Propensity for Violence",
+		band: "Antropofago",
+		country: "FR",
+		link: "",
+		style: "Death Brutal",
+		logo_url: "",
+		year: 2018
+	},
+	{
+		name: "Rites",
+		band: "Mysticisme",
+		country: "FR",
+		link: "",
+		style: "Black Atmosphérique",
+		logo_url: "",
+		year: 2016
+	},
+	{
+		name: "Le Naufrage",
+		band: "Opprobre",
+		country: "FR",
+		link: "",
+		style: "Post Black Metal",
+		logo_url: "",
+		year: 2017
+	},
+]
