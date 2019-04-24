980516f61da2b79943da668b0bf1da532eef3cfd
Clément Roig <clm.roig@gmail.com>
2019-04-24 22:55:36 +0200

    display productions, contact layout, change image background

--- data/productions.js
+++ data/productions.js
@@ -1,29 +1,43 @@
-os_productions = [
+var os_productions = [
 	{
 		name: "A Propensity for Violence",
 		band: "Antropofago",
 		country: "FR",
-		link: "",
+		link: "https://antropofago.bandcamp.com/album/a-propensity-for-violence",
 		style: "Death Brutal",
 		logo_url: "",
+		cover_url: "images/covers/Antropofago-A_Propensity_for_Violence.png",
 		year: 2018
 	},
 	{
 		name: "Rites",
 		band: "Mysticisme",
 		country: "FR",
-		link: "",
+		link: "https://endlessdecrepitudeprod.bandcamp.com/album/rites",
 		style: "Black Atmosphérique",
 		logo_url: "",
+		cover_url: "images/covers/Mysticisme-Rites.jpg",
 		year: 2016
 	},
 	{
 		name: "Le Naufrage",
 		band: "Opprobre",
 		country: "FR",
-		link: "",
+		link: "https://endlessdecrepitudeprod.bandcamp.com/album/le-naufrage",
 		style: "Post Black Metal",
 		logo_url: "",
+		cover_url: "images/covers/Opprobre-Le_Naufrage.jpg",
 		year: 2017
 	},
-]
+	{
+		name: "The Perpetual Motion",
+		band: "Devolution",
+		country: "FR",
+		link: "https://www.youtube.com/watch?v=3mD6pFnvNsA",
+		style: "Death Black",
+		logo_url: "",
+		cover_url: "images/covers/Devolution-The_Perpetual_Motion.jpg",
+		year: 2015
+	}
+];
+
