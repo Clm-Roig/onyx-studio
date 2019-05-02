dbd7697f036ee93816b34c2b382dcd147f0f03e4
Clément Roig <clm.roig@gmail.com>
2019-05-02 22:15:26 +0200

    same indent for everything

--- data/os_productions.js
+++ data/os_productions.js
@@ -1,54 +1,54 @@
 var os_productions = [
-	{
-		name: "A Propensity for Violence",
-		band: "Antropofago",
-		country: "FR",
-		link: "https://antropofago.bandcamp.com/album/a-propensity-for-violence",
-		style: "Death Brutal",
-		logo_url: "",
-		cover_url: "images/covers/Antropofago-A_Propensity_for_Violence.png",
-		facebook_url: "https://www.facebook.com/antropofago.deathmetal",
-		youtube_url: "https://www.youtube.com/watch?v=yK160fu2cXI",
-		year: 2018,
-    credits: ['Programmation batterie', 'Mixage']
-	},
-	{
-		name: "Rites",
-		band: "Mysticisme",
-		country: "FR",
-		link: "https://endlessdecrepitudeprod.bandcamp.com/album/rites",
-		style: "Black Atmosphérique",
-		logo_url: "",
-		cover_url: "images/covers/Mysticisme-Rites.jpg",
-		facebook_url: "https://www.facebook.com/Mysticismeband",
-		youtube_url: "https://www.youtube.com/watch?v=RjrnN1vny2o",
-		year: 2016,
-    credits: ['Programmation batterie', 'Réamping', 'Mixage', 'Mastering']
-	},
-	{
-		name: "Le Naufrage",
-		band: "Opprobre",
-		country: "FR",
-		link: "https://endlessdecrepitudeprod.bandcamp.com/album/le-naufrage",
-		style: "Post Black Metal",
-		logo_url: "",
-		cover_url: "images/covers/Opprobre-Le_Naufrage.jpg",
-		facebook_url: "https://www.facebook.com/opprobre",
-		youtube_url: "https://www.youtube.com/watch?v=Nn0V-FKm5KY",
-		year: 2017,
-    credits: ['Programmation batterie', 'Réamping', 'Mixage', 'Mastering']
-	},
-	{
-		name: "The Perpetual Motion",
-		band: "Devolution",
-		country: "FR",
-		link: "https://www.youtube.com/watch?v=3mD6pFnvNsA",
-		style: "Death Black",
-		logo_url: "",
-		cover_url: "images/covers/Devolution-The_Perpetual_Motion.jpg",
-		facebook_url: "https://www.facebook.com/devolution.dbm",
-		youtube_url: "https://www.youtube.com/watch?v=3mD6pFnvNsA",
-		year: 2015,
-    credits: ['Programmation batterie', 'Réamping', 'Mixage', 'Mastering']
-	}
+    {
+        name: "A Propensity for Violence",
+        band: "Antropofago",
+        country: "FR",
+        link: "https://antropofago.bandcamp.com/album/a-propensity-for-violence",
+        style: "Death Brutal",
+        logo_url: "",
+        cover_url: "images/covers/Antropofago-A_Propensity_for_Violence.png",
+        facebook_url: "https://www.facebook.com/antropofago.deathmetal",
+        youtube_url: "https://www.youtube.com/watch?v=yK160fu2cXI",
+        year: 2018,
+        credits: ['Programmation batterie', 'Mixage']
+    },
+    {
+        name: "Rites",
+        band: "Mysticisme",
+        country: "FR",
+        link: "https://endlessdecrepitudeprod.bandcamp.com/album/rites",
+        style: "Black Atmosphérique",
+        logo_url: "",
+        cover_url: "images/covers/Mysticisme-Rites.jpg",
+        facebook_url: "https://www.facebook.com/Mysticismeband",
+        youtube_url: "https://www.youtube.com/watch?v=RjrnN1vny2o",
+        year: 2016,
+        credits: ['Programmation batterie', 'Réamping', 'Mixage', 'Mastering']
+    },
+    {
+        name: "Le Naufrage",
+        band: "Opprobre",
+        country: "FR",
+        link: "https://endlessdecrepitudeprod.bandcamp.com/album/le-naufrage",
+        style: "Post Black Metal",
+        logo_url: "",
+        cover_url: "images/covers/Opprobre-Le_Naufrage.jpg",
+        facebook_url: "https://www.facebook.com/opprobre",
+        youtube_url: "https://www.youtube.com/watch?v=Nn0V-FKm5KY",
+        year: 2017,
+        credits: ['Programmation batterie', 'Réamping', 'Mixage', 'Mastering']
+    },
+    {
+        name: "The Perpetual Motion",
+        band: "Devolution",
+        country: "FR",
+        link: "https://www.youtube.com/watch?v=3mD6pFnvNsA",
+        style: "Death Black",
+        logo_url: "",
+        cover_url: "images/covers/Devolution-The_Perpetual_Motion.jpg",
+        facebook_url: "https://www.facebook.com/devolution.dbm",
+        youtube_url: "https://www.youtube.com/watch?v=3mD6pFnvNsA",
+        year: 2015,
+        credits: ['Programmation batterie', 'Réamping', 'Mixage', 'Mastering']
+    }
 ];
