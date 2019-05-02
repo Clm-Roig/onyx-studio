8b188f8ecad6c6342307f3475f9e5758635260eb
Clément Roig <clm.roig@gmail.com>
2019-05-02 23:55:58 +0200

    longer scroll down/top + add fade up to equipment

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
     "use strict";
     
     var cfg = {
-        scrollDuration : 200, // smoothscroll duration
+        scrollDuration : 400, // smoothscroll duration
         mailChimpURL   : ''   // mailchimp url
     },
 
